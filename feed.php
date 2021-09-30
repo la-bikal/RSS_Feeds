06850b5d1ec165c3c6e1dc498a4e058b0ccd7a76
la-bikal <91643522+la-bikal@users.noreply.github.com>
2021-09-29 21:28:31 -0400

    Update feed.php for latest codes

--- feed.php
+++ feed.php
@@ -1,30 +1,22 @@
-<?php 
-error_reporting(0);
-$feedlist = array(
-    "BBC"=>"http://feeds.bbci.co.uk/news/rss.xml",
-    // "REUTERS"=>"http://feeds.reuters.com/reuters/technologyNews",
+<?php   
+    include('feedHelpers.php');    
+    
+    # List of sources to extract news feeds from
+    $feedlist = array(
+    "BBC"=>"http://feeds.bbci.co.uk/news/rss.xml",    
     "NYT"=>"https://rss.nytimes.com/services/xml/rss/nyt/Technology.xml",
     "CNN"=>"http://rss.cnn.com/rss/cnn_tech.rss"
-);
-
-foreach($feedlist as $x => $x_value) {
-    echo "<i style='color:#FFab54'>[[".$x."]]</i>: ";
-
-    $xmlDoc = new DOMDocument();
-
-    if($xmlDoc->load($x_value)){
-        $x=$xmlDoc->getElementsByTagName('item');
-
-        for ($i=0; $i<5; $i++) {
-
-        $item_desc=$x->item($i)->getElementsByTagName('description')->item(0)->childNodes->item(0)->nodeValue;
-        echo "<span>" . strip_tags($item_desc) . "</span>";
-            }
-    }
-    else{ 
-        echo "<span> No news feed </span>";
-    }
-}
+    ); 
+    
+    # Number of news items to fetch for each source
+    $newsCount = 3;
 ?>
+<html>
+<head>
+    <title> News Feed </title> 
+</head>
 
-
+<body>
+    <marquee><h2><?php echo getFeedsForMarquee($feedlist, $newsCount) ?>  </h2></marquee>
+</body>
+</html>
